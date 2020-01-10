b0df6dbe111f9e28fc3a9c9b841cde5c20c365f9
Linus <linus@example.com>
2020-01-10 14:00:36 -0600

    support wildcards

--- wc.py
+++ wc.py
@@ -1,20 +1,34 @@
-import sys
+import sys, json
+
+def get_words(path):
+    words = []
+    f = open(path, encoding="utf-8")
+    for line in f:
+        words.extend(line.split(" "))
+    f.close()
+    return words
+
+
+def count_words(path, search):
+    count = 0
+    for w in get_words(path):
+        if w == search:
+            count += 1
+    return count
+
 
 def main():
     assert len(sys.argv) == 3 # TODO: give friendly warning instead
     path = sys.argv[1]
-    search = sys.argv[2] # TODO: support wildcards
+    search = sys.argv[2]
 
-    count = 0
-    f = open(path, encoding="utf-8")
-    for line in f:
-        words = line.split(" ")
-        for word in words:
-            if word == search:
-                count += 1
-    f.close()
-
-    print(count)
+    if search.upper() == "ALL":
+        counts = {}
+        for word in get_words(path):
+            counts[word] = count_words(path, word)
+        print(json.dumps(counts, sort_keys=True, indent=2))
+    else:
+        print(count_words(path, search))
 
 
 if __name__ == '__main__':
